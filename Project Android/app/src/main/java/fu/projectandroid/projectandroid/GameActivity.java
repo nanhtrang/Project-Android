fbe330cc2ef339f21955c4dfb3deab66e593f45f
nanhtrang <nanhtrang110397@gmail.com>
2018-10-25 22:18:56 +0700

    toan sửa layout object

--- Project Android/app/src/main/java/fu/projectandroid/projectandroid/GameActivity.java
+++ Project Android/app/src/main/java/fu/projectandroid/projectandroid/GameActivity.java
@@ -1,5 +1,7 @@
 package fu.projectandroid.projectandroid;
 
+import android.os.Handler;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MotionEvent;
@@ -14,6 +16,12 @@ public class GameActivity extends AppCompatActivity {
     private ImageView hero;
     private TextView stick;
 
+
+    //Thread
+    private Thread stickHightUpThread;
+    private Handler stickHightUpHandler;
+    private boolean isTouching = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,17 +32,48 @@ public class GameActivity extends AppCompatActivity {
         stick = findViewById(R.id.stick);
 
         setWall1Location();
+
     }
 
-    public void setWall1Location(){
-        wall2.setX(200);
+    public void setWall1Location() {
+        wall2.setX(400);
     }
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        if (event.getAction() == MotionEvent.ACTION_DOWN){
-            Toast.makeText(this, String.valueOf(stick.getHeight()), Toast.LENGTH_SHORT ).show();
+        if (event.getAction() == MotionEvent.ACTION_DOWN) {
+            //Toast.makeText(this, String.valueOf(stick.getHeight()), Toast.LENGTH_SHORT).show();
+            isTouching = true;
+            stickHightUp();
+        } else if (event.getAction() == MotionEvent.ACTION_UP) {
+            isTouching = false;
         }
         return super.onTouchEvent(event);
     }
+
+    public void stickHightUp() {
+        stickHightUpHandler = new Handler();
+        stickHightUpThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                while (isTouching) {
+                    stickHightUpHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+                            stick.setHeight(stick.getHeight()+8);
+                            stick.setY(stick.getY() - 3);
+
+                        }
+                    });
+                    try {
+                        Thread.sleep(7);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+        stickHightUpThread.start();
+    }
+
 }
