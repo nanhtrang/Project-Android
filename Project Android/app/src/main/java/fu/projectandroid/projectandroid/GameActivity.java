3d73ef07ae5d77606068b0c9a6b79b385ce33104
nanhtrang <nanhtrang110397@gmail.com>
2018-10-25 19:36:38 +0700

    toantd add object

--- Project Android/app/src/main/java/fu/projectandroid/projectandroid/GameActivity.java
+++ Project Android/app/src/main/java/fu/projectandroid/projectandroid/GameActivity.java
@@ -2,38 +2,38 @@ package fu.projectandroid.projectandroid;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    //object
-    private ImageView hero;
     private TextView wall1;
     private TextView wall2;
+    private ImageView hero;
     private TextView stick;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
-        //get id for object do
-        hero = findViewById(R.id.hero);
         wall1 = findViewById(R.id.wall1);
         wall2 = findViewById(R.id.wall2);
+        hero = findViewById(R.id.hero);
         stick = findViewById(R.id.stick);
-        //end
 
-        //khoi tao vi tri va do cao ban dau ban dau cho stick
-        stickConstructor();
-        //end
-
+        setWall1Location();
     }
 
-    //khoi tao vi tri va do cao ban dau ban dau cho stick
-    public void stickConstructor(){
-        stick.setHeight(1);
-        stick.setX(stick.getX()+100);
+    public void setWall1Location(){
+        wall2.setX(150);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        if (event.getAction() == MotionEvent.ACTION_DOWN){
+
+        }
+        return super.onTouchEvent(event);
     }
 }
